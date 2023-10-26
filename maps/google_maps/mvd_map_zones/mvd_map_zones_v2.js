039831f68422c2e1ddad84a482dcdb629b97cc85
Cristian Encalada <77868316+cristian-encalada@users.noreply.github.com>
2023-10-26 15:06:02 -0300

    mvd_zone_map v2 - Add info window to each polygon

--- maps/google_maps/mvd_map_zones/mvd_map_zones_v2.js
+++ maps/google_maps/mvd_map_zones/mvd_map_zones_v2.js
@@ -1,11 +1,10 @@
 async function initMap() {
   // Request needed libraries.
-  const { Map } = await google.maps.importLibrary("maps");
+  const { Map, InfoWindow } = await google.maps.importLibrary("maps");
   var map = new google.maps.Map(document.getElementById('map'), {
-      center: { lat: -34.901112, lng: -56.164532 },
-      zoom: 13
+    center: { lat: -34.901112, lng: -56.164532 },
+    zoom: 13
   });
-
 
   // Fetch the coordinates from 'mvd_coordinates' CSV file
   const response = await fetch('mvd_coordinates.csv');
@@ -25,32 +24,43 @@ async function initMap() {
   });
 
   for (const name in polygons) {
-    addPolygon(map, polygons[name]);
+    addPolygon(map, polygons[name], name);
   }
 }
 
-function addPolygon(map, coordinates) {
-  var polygon = new google.maps.Polygon({
-      paths: coordinates,
-      strokeColor: '#FF0000',
-      strokeOpacity: 0.8,
-      strokeWeight: 2,
-      fillColor: 'transparent',
-      fillOpacity: 0.35
+function addPolygon(map, coordinates, name) {
+  const polygon = new google.maps.Polygon({
+    paths: coordinates,
+    strokeColor: '#FF0000',
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: 'transparent',
+    fillOpacity: 0.35
   });
 
   polygon.setMap(map);
 
-  // mouseover and mouseout event listeners
+  // Create an InfoWindow with the zone name
+  const infoWindow = new google.maps.InfoWindow({
+    content: name,
+  });
+
+  // Calculate the center of the polygon
+  const bounds = new google.maps.LatLngBounds();
+  coordinates.forEach(coord => bounds.extend(new google.maps.LatLng(coord.lat, coord.lng)));
+  const polygonCenter = bounds.getCenter();
+
+  // Add mouseover and mouseout event listeners
   polygon.addListener('mouseover', function() {
     polygon.setOptions({ fillColor: '#FF0000' });
+    infoWindow.setPosition(polygonCenter);
+    infoWindow.open(map);
   });
 
   polygon.addListener('mouseout', function() {
     polygon.setOptions({ fillColor: 'transparent' });
-  
+    infoWindow.close();
   });
-
 }
 
 initMap();
