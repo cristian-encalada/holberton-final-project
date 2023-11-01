4cc3555f9255d71175be8058f37d4661692f72bc
Cristian Encalada <77868316+cristian-encalada@users.noreply.github.com>
2023-10-31 21:46:41 -0300

    Rentals map, complete for deploy

--- index.js
+++ index.js
@@ -1,131 +1,142 @@
+let allRentsData; // Variable to store the fetched data
+
+async function fetchData() {
+  const response = await fetch("https://alquivago-flask-apis.vercel.app/api/v1/rent/mapa/todo");
+  allRentsData = await response.json();
+  // Store the data in local storage
+  localStorage.setItem("rentData", JSON.stringify(allRentsData));
+}
+
+// Check if data is already in local storage and fetch if not
+if (localStorage.getItem("rentData")) {
+  allRentsData = JSON.parse(localStorage.getItem("rentData"));
+} else {
+  fetchData();
+}
+
 async function initMap() {
   // Request needed libraries.
   const { Map } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary(
-    "marker",
-  );
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker"); // Import AdvancedMarkerElement
   const map = new Map(document.getElementById("map"), {
-    center: { lat: -34.901112, lng: -56.164532 },
-    zoom: 14,
+    center: { lat: -34.8167, lng: -56.1833 }, // Peñarol - Zoom 12
+    zoom: 12,
     mapId: "16aaecb986532f9a",
   });
 
   let markers = []; // Store created markers in an array
 
-  const createRedCircleMarker = (position) => {
-    // A marker with a custom inline SVG for the red circle icon.
-    const redCircleSvgString = '<svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 20 20"><circle cx="10" cy="10" r="8" fill="red" /></svg>';
-    const redCircleSvg = new DOMParser().parseFromString(redCircleSvgString, "image/svg+xml").documentElement;
-
-    // Create a marker with the red circle icon
+  function createBlueCircleMarker(position) {
+    // Create blue circle markers
+    const blueCircleSvgString = '<svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 20 20"><circle cx="10" cy="10" r="8" fill="blue" /></svg>';
+    const blueCircleSvg = new DOMParser().parseFromString(blueCircleSvgString, "image/svg+xml").documentElement;
+  
     const marker = new AdvancedMarkerElement({
       map,
       position,
-      content: redCircleSvg,
-      title: "Red Circle Marker",
+      content: blueCircleSvg,
+      title: "Zoom in to check the price",
     });
-
+  
     markers.push(marker); // Store the created marker in the array
+  }
+
+  const infoWindows = []; // Store created InfoWindows in an array
+
+  const createInfoWindow = (position, priceInfo, url_link, origin, zone) => {
+  // Create an InfoWindow to display price information and URL link
+  const content = `<div><strong>${priceInfo}</strong></div>
+  <div onmouseover="(() => {
+    if (!this.hasAppendedLink) {
+        var linkDiv = document.createElement('div');
+        var link = document.createElement('a');
+        link.href = '${url_link}';
+        link.target = '_blank'; // Open the URL in a new tab
+        link.textContent = 'URL: ${url_link}';
+        linkDiv.appendChild(link);
+        this.appendChild(linkDiv);
+        var originDiv = document.createElement('div');
+        originDiv.textContent = 'Fuente: ${origin}';
+        this.appendChild(originDiv);
+        var zoneDiv = document.createElement('div');
+        zoneDiv.textContent = 'Zona: ${zone}';
+        this.appendChild(zoneDiv);
+        this.hasAppendedLink = true; // Set the flag to true to prevent further appends
+    }
+  })()">Ver detalles</div>`;
+    const infoWindow = new google.maps.InfoWindow({
+      position,
+      content: content,
+      disableAutoPan: true, // Disable automatic centering
+    });
+  
+    // Open the InfoWindow by default
+    infoWindow.open(map);
+  
+    infoWindows.push(infoWindow); // Store the created InfoWindow in the array
   };
 
   const handleZoomChange = () => {
     const zoom = map.getZoom();
     console.log(`Zoom level: ${zoom}`); // Log the zoom level
-
+  
+    // Close existing InfoWindows
+    infoWindows.forEach((infoWindow) => {
+      infoWindow.close();
+    });
+    infoWindows.length = 0; // Clear the InfoWindows array
+  
     // Remove existing markers from the map
     markers.forEach((marker) => {
       marker.setMap(null);
     });
-    markers = []; // Clear the markers array
-
+    markers.length = 0; // Clear the markers array
+  
     if (zoom >= 14) {
-      // Fetch data from the API endpoint
-      fetch("https://alquivago-flask-apis.vercel.app/api/v1/rent/mapa")
-        .then((response) => response.json())
-        .then((data) => {
-          if (data && data.rents && Array.isArray(data.rents)) {
-            data.rents.forEach((rent) => {
-              if (rent.location && rent.location.latitude && rent.location.longitude) {
-                const glyphImg = document.createElement("img");
-                let iconSrc;
-
-                // Determine the icon source based on the "origin" field
-                switch (rent.origin) {
-                  case "gallito":
-                    iconSrc = "https://raw.githubusercontent.com/cristian-encalada/Alquivago/maps/maps/google_maps/assets/gallito_logo.svg";
-                    break;
-                  case "infocasas":
-                    iconSrc = "https://raw.githubusercontent.com/cristian-encalada/Alquivago/maps/maps/google_maps/assets/infocasas_logo.svg";
-                    break;
-                  case "mercado_libre":
-                    iconSrc = "https://raw.githubusercontent.com/cristian-encalada/Alquivago/maps/maps/google_maps/assets/mercado_libre_logo.svg";
-                    break;
-                  default:
-                    // Use a default icon if "origin" doesn't match any of the cases
-                    iconSrc = "default-icon-url.png";
-                }
-
-                glyphImg.src = iconSrc;
-                glyphImg.width = 20;
-                glyphImg.height = 20;
-
-                // Create a marker with the determined icon source
-                const glyphSvgPinElement = new PinElement({
-                  glyph: glyphImg,
-                });
-
-                // Create a marker
-                const marker = new AdvancedMarkerElement({
-                  map,
-                  position: {
-                    lat: rent.location.latitude,
-                    lng: rent.location.longitude,
-                  },
-                  content: glyphSvgPinElement.element,
-                  title: rent.title,
-                });
-
-                // Determine the price format based on the currency
-                let priceInfo;
-                if (rent.currency === "UYU") {
-                  priceInfo = `<div><strong>$ ${rent.price} ${rent.currency}</strong></div>`;
-                } else if (rent.currency === "USD") {
-                  priceInfo = `<div><strong>U$S ${rent.price} ${rent.currency}</strong></div>`;
-                } else {
-                  // Use a default format if currency is neither UYU nor USD
-                  priceInfo = `<div><strong>${rent.price} ${rent.currency}</strong></div>`;
-                }
-
-                // Create an InfoWindow to display price information
-                const infoWindow = new google.maps.InfoWindow({
-                  content: priceInfo,
-                });
-
-                // Open the InfoWindow by default for each marker
-                infoWindow.open(map, marker);
-
-                markers.push(marker); // Store the created marker in the array
-              }
-            });
+      if (allRentsData && allRentsData.rents) {
+        allRentsData.rents.forEach((rent) => {
+          if (rent.location && rent.location.latitude && rent.location.longitude) {
+            // Determine the price format based on the currency
+            let priceInfo;
+            if (rent.currency === "UYU") {
+              priceInfo = `<div><strong>$ ${rent.price} ${rent.currency}</strong></div>`;
+            } else if (rent.currency === "USD") {
+              priceInfo = `<div><strong>U$S ${rent.price} ${rent.currency}</strong></div>`;
+            } else {
+              // Use a default format if currency is neither UYU nor USD
+              priceInfo = `<div><strong>${rent.price} ${rent.currency}</strong></div>`;
+            }
+            let url_link;
+            url_link = rent.url_link; 
+            let origin;
+            origin = rent.origin;
+            let zone;
+            zone = rent.zone_name;
+            createInfoWindow(
+              {
+                lat: rent.location.latitude,
+                lng: rent.location.longitude,
+              },
+              priceInfo,
+              url_link,
+              origin,
+              zone
+            );
           }
         });
+      }
     } else if (zoom < 14) {
-      // Fetch data from the API endpoint when zoom is less than 14
-      fetch("https://alquivago-flask-apis.vercel.app/api/v1/rent/mapa")
-        .then((response) => response.json())
-        .then((data) => {
-          if (data && data.rents && Array.isArray(data.rents)) {
-            data.rents.forEach((rent) => {
-              if (rent.location && rent.location.latitude && rent.location.longitude) {
-                // Create a red circle marker for each item
-                createRedCircleMarker({
-                  lat: rent.location.latitude,
-                  lng: rent.location.longitude,
-                });
-              }
+      if (allRentsData && allRentsData.rents) {
+        allRentsData.rents.forEach((rent) => {
+          if (rent.location && rent.location.latitude && rent.location.longitude) {
+            // Create a blue circle marker for each item
+            createBlueCircleMarker({
+              lat: rent.location.latitude,
+              lng: rent.location.longitude,
             });
           }
         });
+      }
     }
   };
 
